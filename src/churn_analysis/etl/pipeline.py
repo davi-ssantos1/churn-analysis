42607c32d5a79d46fff7be5c678013fd7dce62d2
Davi <davi.ssantosads2@gmail.com>
2026-06-03 18:12:59 -0300

    refactor(etl): improve the reusability of the replace_missing_values_in_chunks function

--- src/churn_analysis/etl/pipeline.py
+++ src/churn_analysis/etl/pipeline.py
@@ -4,7 +4,9 @@ from churn_analysis.etl import replace_missing_values_in_chunks, validate_data
 
 def main() -> int:
     for df_chunk in replace_missing_values_in_chunks(
-        raw_data_path=RAW_DATA_PATH, extraction_func=validate_data
+        raw_data_path=RAW_DATA_PATH,
+        extraction_func=validate_data,
+        col_title="total_charges",
     ):
         print(df_chunk)
     return 0
--- src/churn_analysis/etl/transform.py
+++ src/churn_analysis/etl/transform.py
@@ -9,19 +9,16 @@ def replace_missing_values_in_chunks(
     *,
     raw_data_path: Path,
     extraction_func: Callable[[Path], Generator[pd.DataFrame, None, None]],
+    col_title: str,
 ) -> Generator[pd.DataFrame, None, None]:
     """
     A function that transforms missing values into the overall median of the data.
     """
     all_total_charges = []
     for chunk_df in extraction_func(raw_data_path):
-        all_total_charges.extend(
-            chunk_df["total_charges"].dropna(inplace=False).tolist()
-        )
+        all_total_charges.extend(chunk_df[col_title].dropna(inplace=False).tolist())
     total_charges_median = np.median(all_total_charges)
 
     for chunk_df in extraction_func(raw_data_path):
-        chunk_df["total_charges"] = chunk_df["total_charges"].fillna(
-            total_charges_median
-        )
+        chunk_df[col_title] = chunk_df[col_title].fillna(total_charges_median)
         yield chunk_df
